238120a21905b6266d472e6c0f4dd9ff7aed0a95
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2025-02-12 23:07:26 +0300

    v3 новый алгоритм и интерфейс

--- NetGame3/const.py
+++ NetGame3/const.py
@@ -1,18 +1,11 @@
 class Const:
-    VERSION = 'v3.0'
     restart = 1
-    WIDTH = 3000  # Кратно STEP
-    HEIGHT = 3000  # Кратно STEP
-    STEP = 10
-    RADIUS = 10
-    COUNT = 2
-    SIZE_MUL = 2
-    MIN_SAFE_LENGTH = 30
-    START_LIFE = 10
+    WIDTH = 1600
+    HEIGHT = 900
     data = dict()
     data['HOST'] = ''
     data['PORT'] = 5885
     data['GAME_TIMER'] = 120
-    data['BOTS_COUNTER'] = 30
-    data['DATA_WIND'] = 8192
-    data['CHAOS'] = True
+    data['BOTS_COUNTER'] = 6
+    data['STEP_WAIT'] = 0
+    data['DATA_WIND'] = 2048
