fc573fd2be957898366a5864641e33844d16fb0d
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2025-02-17 00:28:43 +0300

    v3.03 Оптимизация пакета данных

--- NetGame3/cl_color.py
+++ NetGame3/cl_color.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3
 # coding:utf-8
 import colorsys
+import os
 import random
+import sys
+
+import pygame
 
 
 class Color:
-    data = [0.1, 0.95, 0.2, 0, 85, 0.3, 0.75, 0.4, 0.65, 0.5,
-            0.55, 0.6, 0.45, 0.7, 0.35, 0.8, 0.25, 0.9, 0.15]
+    # data = [0.1, 0.95, 0.2, 0, 85, 0.3, 0.75, 0.4, 0.65, 0.5,
+    #         0.55, 0.6, 0.45, 0.7, 0.35, 0.8, 0.25, 0.9, 0.15]
+    data = [0.1, 0.2, 0.3, 0.4, 0.5,
+            0.6, 0.7, 0.8, 0.9]
     random.shuffle(data)
     count = 0
 
     def __init__(self):
-        h, s, l = Color.data[Color.count], 0.9, 0.4 + random.random() / 5.0
+        h, s, l = Color.data[Color.count], 0.95, 0.4 + random.random() / 5.0
         r, g, b = [int(256 * i) for i in colorsys.hls_to_rgb(h, l, s)]
         self.color = (r, g, b)
         Color.count = (Color.count + 1) % len(Color.data)
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
--- NetGame3/client_0.py
+++ NetGame3/client_0.py
@@ -12,7 +12,7 @@ from random import choice
 import msgpack
 import pygame
 
-from NetGame3.cl_color import Color
+from NetGame3.cl_color import Color, load_image
 from const import Const
 
 
@@ -40,23 +40,6 @@ else:
     screen = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF)
 clock = pygame.time.Clock()
 pygame.display.set_caption(f'ЗМЕИНЫЕ ГОНКИ. {Const.VERSION} (клиент)')
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
 
 
 class MainMenu:
